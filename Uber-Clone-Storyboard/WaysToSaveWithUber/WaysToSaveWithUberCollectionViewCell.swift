921f46696db588292bef0f984cbaf95a0b2b19f8
Swantan Barua <work.swantan@gmail.com>
2023-06-17 12:21:18 +0530

    Collection view setup

---
+++ Uber-Clone-Storyboard/WaysToSaveWithUber/WaysToSaveWithUberCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  WaysToSaveWithUberCollectionViewCell.swift
+//  Uber-Clone-Storyboard
+//
+//  Created by Swantan Barua on 17/06/23.
+//
+
+import UIKit
+
+class WaysToSaveWithUberCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
